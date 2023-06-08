c57d649d57959ff94d34ed000f0e5edf08e29412
HongGeun <ghdrms163@gmail.com>
2023-06-09 04:16:35 +0900

    [feat] 기능 추가 / refactoring 필요

--- board/src/main/java/com/example/board/BoardApplication.java
+++ board/src/main/java/com/example/board/BoardApplication.java
@@ -3,9 +3,11 @@ package com.example.board;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @MapperScan(value = {"com.example.board.mapper"})
 @SpringBootApplication
+@EnableScheduling
 public class BoardApplication {
 
     public static void main(String[] args) {
--- board/src/main/java/com/example/board/server/post/dao/RedisPostDao.java
+++ board/src/main/java/com/example/board/server/post/dao/RedisPostDao.java
@@ -2,16 +2,21 @@ package com.example.board.server.post.dao;
 
 import com.example.board.server.post.PostReturn;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SetOperations;
 import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class RedisPostDao {
     private final RedisTemplate<String, PostReturn> redisTemplate;
 
@@ -37,5 +42,14 @@ public class RedisPostDao {
 
     public void deleteValues(String key) {
         redisTemplate.delete(key);
+    }
+
+    @Scheduled(cron = "1 * * * * ?")
+    public void applyViewCountToRDB() {
+        Set<String> keys = redisTemplate.keys("*");
+
+        for (String key : keys) {
+            log.info(getValues(key).toString());
+        }
     }
 }
